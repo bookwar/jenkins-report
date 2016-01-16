6711a97fed02b8b80d0453f0cfdec3c8d3cbf617
Aleksandra Fedorova <afedorova@mirantis.com>
2016-01-16 20:01:05 +0300

    Remove unused configuration
    Proper logging for real world deployments should be configured later.

--- JR/__init__.py
+++ JR/__init__.py
@@ -6,15 +6,5 @@ app = flask.Flask(__name__)
 app.config.from_object('config')
 app.config.from_envvar('JENKINS_REPORT_SETTINGS', silent=True)
 
-if not app.debug:
-    file_handler = logging.FileHandler('error.log')
-    file_handler.setFormatter(
-        logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
-    )
-    app.logger.setLevel(logging.INFO)
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-    app.logger.info('errors')
-
 import JR.views
 import JR.filters
